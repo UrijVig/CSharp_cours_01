82a76452bb04052b31a7195ea980c18cb581132b
Yuriy Gelvih <Gelvihurij@gmail.com>
2023-02-18 12:06:31 +0700

    initial commit

---
+++ Seminar01/Demo/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Квадрат вашего числа равен:  {number * number}" );
---
+++ Seminar01/task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if ((numberTwo * numberTwo) == numberOne)
+    Console.WriteLine($"{numberOne} является квадратом числа {numberTwo}" );
+else
+    Console.WriteLine($"{numberOne} не является квадратом числа {numberTwo}" );
+
+
+
+
