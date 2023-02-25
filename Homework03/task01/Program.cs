f51eba2b06b37cdf092395b998e2e31ec1e5d181
Yuriy Gelvih <Gelvihurij@gmail.com>
2023-02-25 10:25:53 +0700

    Homework03

---
+++ Homework03/task01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.Write("Пожалуйста введите пятизначное число:");
+string? input = Console.ReadLine();
+int[] inputArr = new int[input.Length];
+int[] reverseArr = new int[input.Length];
+int count = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    inputArr[i] = Convert.ToInt32(Convert.ToString(input[i]));
+    reverseArr[input.Length - 1 - i] = Convert.ToInt32(Convert.ToString(input[i]));
+}
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (reverseArr[i] == inputArr[i])
+        count++;
+}
+
+if (count++ == input.Length)
+    System.Console.WriteLine("Число является полинромом!");
+else
+    System.Console.WriteLine("Число не является полинромом!");
---
+++ Homework03/task02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+System.Console.WriteLine("Введите X Y Z первой точки");
+int[] coordsA = Array.ConvertAll(Console.ReadLine()!.Split(" "), int.Parse);
+System.Console.WriteLine("Введите X Y Z второй точки");
+int[] coordsB = Array.ConvertAll(Console.ReadLine()!.Split(" "), int.Parse);
+
+double distance = Math.Sqrt(Math.Pow(coordsA[0] - coordsB[0], 2) + Math.Pow(coordsA[1] - coordsB[1], 2) + Math.Pow(coordsA[2] - coordsB[2], 2));
+
+System.Console.WriteLine("Расстояние между точками: " + Math.Round(distance, 2));
