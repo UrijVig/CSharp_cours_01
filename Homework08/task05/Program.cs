cec29430673bacf93b0ce1045ba8173b0f4c2239
UrijVig <118144791+UrijVig@users.noreply.github.com>
2023-07-19 17:47:49 +0700

    Update Program.cs

--- Homework08/task05/Program.cs
+++ Homework08/task05/Program.cs
@@ -1,53 +1,69 @@
-﻿// Напишите программу, которая заполнит спирально массив 4 на 4.
+using System;
+class Program
+{
+// Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
 // 01 02 03 04
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
 
-int ReadInt(string text)
+static int ReadInt(string text)
 {
     System.Console.WriteLine(text);
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int[,] GenerateMatrix(int m, int n)
+static int[,] GenerateMatrix(int m, int n)
 {
     int[,] matrix = new int[m, n];
-    int count = 0;
-    while (count <= matrix.GetLength(0) * matrix.GetLength(1))
+    for (int k = 1; k <= matrix.GetLength(0) * matrix.GetLength(1);) 
     {
-        for (int j = matrix.GetLength(1) - n; j < n; j++)
-        {
-            count++;
-            matrix[matrix.GetLength(0) - m, j] = count;
-        }
-        if (count >= matrix.GetLength(0) * matrix.GetLength(1)) break;
-        for (int i = matrix.GetLength(0) - m + 1; i < m - 1; i++)
-        {
-            count++;
-            matrix[i, n - 1] = count;
-        }
-        if (count >= matrix.GetLength(0) * matrix.GetLength(1)) break;
-        for (int j = n - 1; j > matrix.GetLength(1) - n; j--)
-        {
-            count++;
-            matrix[m - 1, j] = count;
-        }
-        if (count >= matrix.GetLength(0) * matrix.GetLength(1)) break;
-        for (int i = m - 1; i > matrix.GetLength(0) - m; i--)
-        {
-            count++;
-            matrix[i, matrix.GetLength(1) - n] = count;
-        }
-        if (count >= matrix.GetLength(0) * matrix.GetLength(1)) break;
-        n--;
+        for (int j = matrix.GetLength(1) - n; j < n; j++) 
+            matrix[matrix.GetLength(0) - m, j] = k++;
+        for (int i = matrix.GetLength(0) - m + 1; i < m - 1; i++) 
+            matrix[i, n - 1] = k++;
+        for (int j = n - 1; j > matrix.GetLength(1) - n; j--) 
+            matrix[m - 1, j] = k++;
+        for (int i = m - 1; i > matrix.GetLength(0) - m; i--) 
+            matrix[i, matrix.GetLength(1) - n] = k++;
+        n--; 
         m--;
     }
+    // int count = 0;
+    // while (count <= matrix.GetLength(0) * matrix.GetLength(1))
+    // {
+    //     for (int j = matrix.GetLength(1) - n; j < n; j++)
+    //     {
+    //         count++;
+    //         matrix[matrix.GetLength(0) - m, j] = count;
+    //     }
+    //     // if (count >= matrix.GetLength(0) * matrix.GetLength(1)) break;
+    //     for (int i = matrix.GetLength(0) - m + 1; i < m - 1; i++)
+    //     {
+    //         count++;
+    //         matrix[i, n - 1] = count;
+    //     }
+    //     // if (count >= matrix.GetLength(0) * matrix.GetLength(1)) break;
+    //     for (int j = n - 1; j > matrix.GetLength(1) - n; j--)
+    //     {
+    //         count++;
+    //         matrix[m - 1, j] = count;
+    //     }
+    //     // if (count >= matrix.GetLength(0) * matrix.GetLength(1)) break;
+    //     for (int i = m - 1; i > matrix.GetLength(0) - m; i--)
+    //     {
+    //         count++;
+    //         matrix[i, matrix.GetLength(1) - n] = count;
+    //     }
+    //     if (count >= matrix.GetLength(0) * matrix.GetLength(1)) break;
+    //     n--;
+    //     m--;
+    // }
     return matrix;
 }
 
-void PrintMatrix(int[,] matr)
+static void PrintMatrix(int[,] matr)
 {
     for (int i = 0; i < matr.GetLength(0); i++)
     {
@@ -58,9 +74,12 @@ void PrintMatrix(int[,] matr)
         System.Console.WriteLine();
     }
 }
-
-int m = ReadInt("Введите количество строк матрицы: ");
-int n = ReadInt("Введите количество столбцов матрицы: ");
-
-var myMatrix = GenerateMatrix(m, n);
-PrintMatrix(myMatrix);
+  static void Main() {
+    int m = ReadInt("Введите количество строк матрицы: ");
+    int n = ReadInt("Введите количество столбцов матрицы: ");
+    
+    var myMatrix = GenerateMatrix(m, n);
+    PrintMatrix(myMatrix);
+    
+  }
+}
